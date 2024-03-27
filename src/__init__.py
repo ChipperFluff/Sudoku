3ae9dcfbe084ce19c9ab2a9e4f29416a26f2e009
TheNestingDev <jack.squirrel.live@gmail.com>
2024-03-27 17:30:55 +0100

    prepare for test run in exe

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,8 @@ from .game_logic import Board, Vec2
 from .game_visual import Game
 
 def main():
-    num_sections = Vec2(4, 2)
-    section_dimensions = Vec2(2, 4)
+    num_sections = Vec2(3, 3)
+    section_dimensions = Vec2(3, 3)
     board = Board(num_sections, section_dimensions)
     game = game_visual.Game(board)
     game.run()
--- src/game_visual.py
+++ src/game_visual.py
@@ -1,20 +1,21 @@
 import arcade
 from .game_logic import Board, Cell, Vec2
+from typing import List, Dict, Union
 
 class CellSprite(arcade.Sprite):
     SIZE = None
 
     def __init__(self, center_x:float, center_y:float, logic_cell:Cell, texture_setting:dict):
-        texture_types = {"corner": "src/resources/imgs/cell_corner.png",
-                         "border": "src/resources/imgs/cell_border.png",
-                         "mid": "src/resources/imgs/cell_mid.png"}
+        self.logic_cell = logic_cell
 
-        super().__init__(filename=texture_types[texture_setting["type"]],
+        locked = "close" if self.logic_cell.locked else "open"
+        texture_path = f'src/resources/imgs/cell_{texture_setting["type"]}_{locked}.png'
+
+        super().__init__(filename=texture_path,
                          center_x=center_x,
                          center_y=center_y)
 
         self.angle = texture_setting["rotation"]
-        self.logic_cell = logic_cell
         self.width = self.SIZE.x + 5
         self.height = self.SIZE.x + 5
 
@@ -24,9 +25,11 @@ class Game(arcade.Window):
 
         self.board:Board = board
         self.cell_sprites = []
+        self.board_dimensions:Vec2 = None
+        self.cell_size:Vec2 = None
         self._create_board()
 
-    def _create_board(self):
+    def _calculate_board_size(self):
         MARGIN_PERCENTAGES_VERTICAL = 400
         MARGIN_PERCENTAGES_HORIZONTAL = 600
 
@@ -35,54 +38,66 @@ class Game(arcade.Window):
 
         field_start = Vec2(horizontal_margin, self.height-vertical_margin) # top left corner
         field_stop = Vec2(self.width-horizontal_margin, vertical_margin) # bottom right corner
+        self.board_dimensions = Vec2(field_start, field_stop)
 
         field_size = Vec2(field_stop.x - field_start.x,
                           field_stop.y - field_start.y)
 
         total_cells = self.board.total_board_size
 
-        cell_size = Vec2(field_size.x / total_cells.x,
+        self.cell_size = Vec2(field_size.x / total_cells.x,
                          field_size.y / total_cells.y)
-        CellSprite.SIZE = cell_size
+        CellSprite.SIZE = self.cell_size
 
-        Vec2.apply_stack(field_start, field_stop, field_size, cell_size)
+        Vec2.apply_stack(field_start, field_stop, field_size, self.cell_size)
 
-        x_count = 0
-        y_count = 0
+    def _choose_cell_texture(self, x_pos:int, y_pos:int) -> Dict[str, Union[str, int]]:
         section_dimensions = self.board.section_dimensions
 
-        for index_y, y in enumerate(range(int(field_start.y+(cell_size.y/2)), int(field_stop.y), cell_size.y)):
+        # Check for corners first
+        if (x_pos, y_pos) == (1, 1):
+            return {"type": "corner", "rotation": 0}
+        elif (x_pos, y_pos) == (section_dimensions.x, 1):
+            return {"type": "corner", "rotation": -90}
+        elif (x_pos, y_pos) == (1, section_dimensions.y):
+            return {"type": "corner", "rotation": 90}
+        elif (x_pos, y_pos) == (section_dimensions.x, section_dimensions.y):
+            return {"type": "corner", "rotation": 180}
+
+        # Check for edge
+        elif x_pos == 1:
+            return {"type": "edge", "rotation": 0}
+        elif x_pos == section_dimensions.x:
+            return {"type": "edge", "rotation": 180}
+        elif y_pos == 1:
+            return {"type": "edge", "rotation": -90}
+        elif y_pos == section_dimensions.y:
+            return {"type": "edge", "rotation": 90}
+
+        # Default to mid piece if not a edge or corner
+        return {"type": "mid", "rotation": 0}
+
+
+    def _create_board(self):
+        self._calculate_board_size()
+        section_dimensions = self.board.section_dimensions
+
+        y_count = 0
+        for index_y, y in enumerate(range(int(self.board_dimensions.x.y+(self.cell_size.y/2)), int(self.board_dimensions.y.y), self.cell_size.y)):
             y_count +=1
-            for index_x, x in enumerate(range(int(field_start.x+(cell_size.x/2)), int(field_stop.x), cell_size.x)):
+            x_count = 0
+
+            for index_x, x in enumerate(range(int(self.board_dimensions.x.x+(self.cell_size.x/2)), int(self.board_dimensions.y.x), self.cell_size.x)):
                 x_count += 1
-                texture_setting = {"type": "mid", "rotation": 0}
-
-                # border
-                if x_count == 1:
-                    texture_setting = {"type": "border", "rotation": 0}
-                elif x_count == section_dimensions.x:
-                    texture_setting = {"type": "border", "rotation": 180}
-                elif y_count == 1:
-                    texture_setting = {"type": "border", "rotation": -90}
-                elif y_count == section_dimensions.y:
-                    texture_setting = {"type": "border", "rotation": 90}
-
-                # corner
-                if x_count == y_count == 1:
-                    texture_setting = {"type": "corner", "rotation": 0}
-                elif x_count == section_dimensions.x and y_count == 1:
-                    texture_setting = {"type": "corner", "rotation": -90}
-                elif x_count == 1 and y_count == section_dimensions.y:
-                    texture_setting = {"type": "corner", "rotation": 90}
-                elif x_count == section_dimensions.x and y_count == section_dimensions.y:
-                    texture_setting = {"type": "corner", "rotation": 180}
 
                 logic_cell:Cell = self.board.active_cells[(index_x, index_y)]
+                texture_setting = self._choose_cell_texture(x_count, y_count)
                 sprite_cell = CellSprite(x, y, logic_cell, texture_setting)
                 self.cell_sprites.append(sprite_cell)
 
                 if x_count >= section_dimensions.x:
                     x_count = 0
+
             if y_count >= section_dimensions.y:
                     y_count = 0
 
