e78dcf1558fd4910d825549037d2269f22417451
lordlogo2002 <i96774080@gmail.com>
2024-04-08 08:52:41 +0200

    made ui and window class like said in #1

--- src/ui_manager.py
+++ src/ui_manager.py
@@ -1,34 +1,69 @@
-from .data_structures import Size
+from __future__ import annotations
+from data_structures import Size, Vec2
 import pygame
 
 
 class View:
     def __init__(self):
+        pass
+
+    def on_update(self):
+        pass
+
+    def on_draw(self):
+        pass
+
+    def on_mouse_click(self, up:bool, pos:Vec2, button:int):
+        pass
+
+    def on_keyboard(self, up:bool, key):
+        pass
+
+    def on_quit(self):
+        pass
 
 class Window:
-    def __init__(self, screen_size:Size):
-        self.screen_size = screen_size
-        self._window_active = True
-        self._active_view:View = None
+    _INSTANCE:Window = None
+
+    def __init__(self, screen_size:Size, title:str, icon_path:str=None, fps:int=60):
+        self._screen_size = screen_size
+        self._title = title
+        self._icon_path = icon_path
+        self._clock = pygame.time.Clock()
         self._create_screen()
 
+        self._window_active = True
+        self._active_view:View = None
+
+        if self._INSTANCE is not None:
+            raise Exception("window class duplicated")
+        self._INSTANCE = self
+
     def _create_screen(self):
-        self.screen = pygame.display.set_mode(self.screen_size.to_tuple)
-        pygame.display.set_caption("Sudok")
-        icon = pygame.image.load('resources/imgs/icon.png')
-        pygame.display.set_icon(icon)
+        self._screen = pygame.display.set_mode(self._screen_size.to_tuple)
+        pygame.display.set_caption(self._title)
+        if self._icon_path is not None:
+            self._icon = pygame.image.load(self._icon_path)
+            pygame.display.set_icon(icon)
 
     def _handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                self._active_view.on_quit()
+                self._window_active = False
+                break
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 position = Vec2(*pygame.mouse.get_pos())
-                if event.button == 1:
-            if event.type == pygame.KEYDOWN:
-                key = event.key
+                self._active_view.on_mouse_click(False, position, event.button)
+            elif event.type == pygame.MOUSEBUTTONUP:
+                position = Vec2(*pygame.mouse.get_pos())
+                self._active_view.on_mouse_click(True, position, event.button)
 
-    def _update(self):
-        pass
+            if event.type == pygame.KEYDOWN:
+                self._active_view.on_keyboard(False, event.key)
+            elif event.type == pygame.KEYUP:
+                self._active_view.on_keyboard(True, event.key)
 
     def _draw(self):
         self.screen.fill((0, 0, 0))  # Black background
@@ -41,10 +76,16 @@ class Window:
                 continue
 
             self._handle_events()
-            self._update()
+            self._active_view.on_update()
             self._draw()
 
     def run(self):
         self._game_loop()
         pygame.quit()
         sys.exit()
+
+def run():
+    window = Window._INSTANCE
+    if window is None:
+        raise TypeError("window not initilized")
+    window.run()
