e2b32daa06db82059994c55e4e20b95e4d839ced
lordlogo2002 <i96774080@gmail.com>
2024-04-09 14:20:10 +0200

    fnished release-1.0.0

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from .data_structures import Board, Size
-from .game_visual import Window
-from .ui_manager import run
-
-def main():
-    screen_size = Size(500, 500)
-    window = Window(screen_size)
-    run()
